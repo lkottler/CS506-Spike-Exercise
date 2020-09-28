70f3292a0dd7d7ee3859d1a16d1b418f0ab6833c
Logan Kottler <lkottler@wisc.edu>
2020-09-28 16:59:48 -0500

    Scripting + Additional Canvas
    I added the first example scripting of using a button to move from one canvas to another.

---
+++ Assets/Scripts/InterfaceLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceLogic : MonoBehaviour
+{
+    public GameObject FrontPage;
+    public GameObject LoginPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FrontPage = GameObject.Find("canvas_FrontPage");
+        LoginPage = GameObject.Find("canvas_LoginPage");
+
+        FrontPage.SetActive(true);
+        LoginPage.SetActive(false);
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void btn_loginScript()
+    {
+        FrontPage.SetActive(false);
+        LoginPage.SetActive(true);
+    }
+
+}
