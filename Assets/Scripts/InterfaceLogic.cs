874a950cc2b3e876e351020434566ade8b604377
logansisel <56094407+logansisel@users.noreply.github.com>
2020-09-28 20:46:33 -0500

    Added scenes, not working currently

--- Assets/Scripts/InterfaceLogic.cs
+++ Assets/Scripts/InterfaceLogic.cs
@@ -1,26 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InterfaceLogic : MonoBehaviour
 {
-    public GameObject FrontPage;
-    public GameObject LoginPage;
-    public GameObject RegisterPage;
-    public GameObject ManagePage;
+    //public GameObject FrontPage;
+    //public GameObject LoginPage;
+    //public GameObject RegisterPage;
+    //public GameObject ManagePage;
 
     // Start is called before the first frame update
     void Start()
     {
-        FrontPage = GameObject.Find("canvas_FrontPage");
-        LoginPage = GameObject.Find("canvas_LoginPage");
-        RegisterPage = GameObject.Find("canvas_RegisterPage");
-        ManagePage = GameObject.Find("canvas_ManagePage");
+        //FrontPage = GameObject.Find("canvas_FrontPage");
+        //LoginPage = GameObject.Find("canvas_LoginPage");
+        //RegisterPage = GameObject.Find("canvas_RegisterPage");
+        //ManagePage = GameObject.Find("canvas_ManagePage");
 
-        FrontPage.SetActive(true);
-        LoginPage.SetActive(false);
-        RegisterPage.SetActive(false);
-        ManagePage.SetActive(false);
+        //SceneManager.LoadScene("InitialPage");
+        //FrontPage.SetActive(true);
+        //LoginPage.SetActive(false);
+        //RegisterPage.SetActive(false);
+        //ManagePage.SetActive(false);
         // Cursor.visible = true;
     }
 
@@ -32,31 +34,35 @@ public class InterfaceLogic : MonoBehaviour
 
     public void btn_loginScript()
     {
-        FrontPage.SetActive(false);
-        LoginPage.SetActive(true);
+        //FrontPage.SetActive(false);
+        //LoginPage.SetActive(true);
+        SceneManager.LoadScene("LoginPage");
     }
 
     public void btn_registerScript()
     {
-        FrontPage.SetActive(false);
-        RegisterPage.SetActive(true);
+        //FrontPage.SetActive(false);
+        //RegisterPage.SetActive(true);
+        SceneManager.LoadScene("RegisterPage");
     }
 
     public void btn_confirm_loginScript()
     {
-        LoginPage.SetActive(false);
+       
+        //LoginPage.SetActive(false);
         //check database for valid entry, load data
 
-        ManagePage.SetActive(true);
-
+        //ManagePage.SetActive(true);
+        SceneManager.LoadScene("ManagePage");
     }
 
     public void btn_confirm_registerScript()
     {
-        LoginPage.SetActive(false);
+        //LoginPage.SetActive(false);
         //check database for valid entry, add entry to database
 
-        ManagePage.SetActive(true);
+        //ManagePage.SetActive(true);
+        SceneManager.LoadScene("ManagePage");
 
     }
 }
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
