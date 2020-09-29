e7af1c46ac4954ae56b5cd83d6d902b8850ff8b0
Logan Kottler <lkottler@wisc.edu>
2020-09-29 11:07:23 -0500

    fixed all bugs, began implementation of database

---
+++ Assets/Scripts/Interface/InitialPage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitialPage : MonoBehaviour
+{
+    public void load_loginPage()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("Attempted to Load Login Page");
+    }
+
+    public void load_registerPage()
+    {
+        Debug.Log("Attempting to load RegisterPage");
+        SceneManager.LoadScene(2);
+    }
+}
