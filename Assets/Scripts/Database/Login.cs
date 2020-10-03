004d83a2b0ee913b44e59cd4285c10ad9d69ae71
Logan Kottler <lkottler@wisc.edu>
2020-10-03 00:03:18 -0500

    fixed login for username less than 8 char

--- Assets/Scripts/Database/Login.cs
+++ Assets/Scripts/Database/Login.cs
@@ -51,7 +51,7 @@ public class Login : MonoBehaviour
     }
     public void VerifyInputs()
     {
-        submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
+        submitButton.interactable = (nameField.text.Length >= 4 && passwordField.text.Length >= 8);
     }
 
 }
